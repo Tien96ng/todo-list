ae7b3ad558f78ea86f1ad122272d6eba1dc09a68
Tien <tien96ng@gmail.com>
2020-04-21 12:01:54 -0700

    Made the functional component to a class component in App.
    Took in the TODODATA as a state.
    
    Fixed the unique key issue! Moved the key to the <TodoItem key={} /> because thats the element that needs it.
    
    The component itself just displays what <TodoItem /> needs!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from "react"
-import TodoItem from "./components/TodoItem"
-import { TODODATA } from "./data/todoData"
+import React from "react";
+import TodoItem from "./components/TodoItem";
+import { TODODATA } from "./data/todoData";
 
-function App() {
-    let renderTodoItem = TODODATA.map(element => {
-      return(
-        <TodoItem 
-          id={element.id} 
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {todoData: TODODATA}
+  }
+
+
+  render() {
+    let renderTodoItem = this.state.todoData.map(element => {
+      return (
+        <TodoItem
+          key={element.id}
           text={element.text}
           completed={element.completed}
         />
-      )
-    })
-    return (
-        <div className="todo-list">
-            {renderTodoItem}
-        </div>
-    )
+      );
+    });
+    return <div className="todo-list">{renderTodoItem}</div>;
+  }
 }
 
-export default App
+export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,14 @@
-import React from "react"
+import React from "react";
 
 function TodoItem(props) {
-    return (
-        <div className="todo-item" key={props.id}>
-            <input type="checkbox" checked={props.completed}/>
-            <p style={{textDecoration: props.completed ? 'line-through' : '' }}>{props.text}</p>
-        </div>
-    )
+  return (
+    <div className="todo-item">
+      <input type="checkbox" />
+      <p style={{ textDecoration: props.completed ? "line-through" : "" }}>
+        {props.text}
+      </p>
+    </div>
+  );
 }
 
-export default TodoItem
+export default TodoItem;
